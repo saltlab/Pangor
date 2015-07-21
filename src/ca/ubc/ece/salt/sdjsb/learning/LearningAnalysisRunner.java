0d417d4972a5729c5bd9cfb45c5621c528ff010c
Fernando Brito <email@fernandobrito.com>
2015-07-21 15:47:49 -0700

    Fix LearningAnalysisRunner not creating folder to output file

--- src/ca/ubc/ece/salt/sdjsb/learning/LearningAnalysisRunner.java
+++ src/ca/ubc/ece/salt/sdjsb/learning/LearningAnalysisRunner.java
@@ -9,16 +9,16 @@ import java.util.List;
 
 import ca.ubc.ece.salt.sdjsb.ControlFlowDifferencing;
 import ca.ubc.ece.salt.sdjsb.analysis.learning.ast.FeatureVector;
-import ca.ubc.ece.salt.sdjsb.analysis.learning.ast.LearningDataSet;
 import ca.ubc.ece.salt.sdjsb.analysis.learning.ast.LearningAnalysis;
+import ca.ubc.ece.salt.sdjsb.analysis.learning.ast.LearningDataSet;
 import ca.ubc.ece.salt.sdjsb.batch.AnalysisMetaInformation;
 import ca.ubc.ece.salt.sdjsb.batch.AnalysisRunner;
 
 public class LearningAnalysisRunner extends AnalysisRunner {
-	
+
 	/** Stores the feature vectors that make up the data set. **/
 	LearningDataSet dataset;
-	
+
 	/**
 	 * @param packages The list of packages we want to learn repair patterns for.
 	 */
@@ -36,33 +36,40 @@ public class LearningAnalysisRunner extends AnalysisRunner {
 
 	@Override
 	public void printResults(String outFile, String supplementaryFolder) {
-		
+
 		/* Filters out unwanted rows and columns. */
 		this.dataset.preProcess();
-		
+
 		/* Open the file stream for writing if a file has been given. */
 		PrintStream stream = System.out;
-		
+
 		if(outFile != null) {
 			try {
+				/*
+				 * The path to the output folder may not exist. Create it if
+				 * needed.
+				 */
+				File path = new File(outFile);
+				path.getParentFile().mkdirs();
+
 				stream = new PrintStream(new FileOutputStream(outFile));
 			}
 			catch (IOException e) {
 				System.err.println(e.getMessage());
 			}
 		}
-		
+
 		/* Write the header for the feature vector. */
 		stream.println(this.dataset.getFeatureVectorHeader());
-		
+
 		/* Write the data set. */
 		stream.println(this.dataset.getFeatureVector());
-		
+
 		/* Write the source code to a folder so we can examine it later. */
 		this.printSupplementaryFiles(supplementaryFolder);
-		
+
 	}
-	
+
 	/**
 	 * Writes the source code from each of the inspected functions to a file.
 	 * @param supplementaryFolder The folder to place the files in.
@@ -70,34 +77,34 @@ public class LearningAnalysisRunner extends AnalysisRunner {
 	private void printSupplementaryFiles(String supplementaryFolder) {
 
 		if(supplementaryFolder != null) {
-			
+
 			for(FeatureVector featureVector : this.dataset.getFeatureVectors()) {
 
-				/* The path to the supplementary folder may not exist. Create 
+				/* The path to the supplementary folder may not exist. Create
 				 * it if needed. */
 				File path = new File(supplementaryFolder);
 				path.mkdirs();
 
 				File src = new File(supplementaryFolder, featureVector.id + "_src.js");
 				File dst = new File(supplementaryFolder, featureVector.id + "_dst.js");
-				
+
 				try (PrintStream srcStream = new PrintStream(new FileOutputStream(src));
 					 PrintStream dstStream = new PrintStream(new FileOutputStream(dst));) {
 
 					srcStream.print(featureVector.sourceCode);
 					dstStream.print(featureVector.destinationCode);
-					
+
 					srcStream.close();
 					dstStream.close();
-					
+
 				} catch (FileNotFoundException e) {
 					System.err.println(e.getMessage());
-				} 
-				
+				}
+
 			}
-			
+
 		}
-		
+
 	}
 
 }
