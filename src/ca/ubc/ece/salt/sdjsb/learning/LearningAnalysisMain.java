790e1a8e5997cee4d2dd9ffe4a3e1bb26fdf5f17
Fernando Brito <email@fernandobrito.com>
2015-07-31 12:40:19 -0700

    Update LearningAnalysis so it doesn't exit on git exceptions

--- src/ca/ubc/ece/salt/sdjsb/learning/LearningAnalysisMain.java
+++ src/ca/ubc/ece/salt/sdjsb/learning/LearningAnalysisMain.java
@@ -15,14 +15,14 @@ public class LearningAnalysisMain {
 
 	/** The directory where repositories are checked out. **/
 	public static final String CHECKOUT_DIR =  new String("repositories");
-	
+
 	/**
 	 * Creates the learning data set for extracting repair patterns.
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		
+
 		LearningAnalysisOptions options = new LearningAnalysisOptions();
 		CmdLineParser parser = new CmdLineParser(options);
 
@@ -32,16 +32,16 @@ public class LearningAnalysisMain {
 			LearningAnalysisMain.printUsage(e.getMessage(), parser);
 			return;
 		}
-		
+
 		/* Print the help page. */
 		if(options.getHelp()) {
 			LearningAnalysisMain.printHelp(parser);
 			return;
 		}
-		
+
 		/* Create the runner that will run the analysis. */
 		LearningAnalysisRunner runner = new LearningAnalysisRunner(options.getDataSetPath(), options.getSupplementaryFolder());
-		
+
         GitProjectAnalysis gitProjectAnalysis;
 
 		/* A URI was given. */
@@ -49,21 +49,21 @@ public class LearningAnalysisMain {
 
 			try {
                 gitProjectAnalysis = GitProjectAnalysis.fromURI(options.getURI(), CHECKOUT_DIR, runner);
-			} 
+			}
 			catch(GitProjectAnalysisException e) {
                 LearningAnalysisMain.printUsage(e.getMessage(), parser);
                 return;
 			}
-			
+
 			gitProjectAnalysis.analyze();
 
 		}
 		/* A list of URIs was given. */
 		else if(options.getRepoFile() != null) {
-			
+
 			/* Parse the file into a list of URIs. */
 			List<String> uris = new LinkedList<String>();
-			
+
 			try(BufferedReader br = new BufferedReader(new FileReader(options.getRepoFile()))) {
 			    for(String line; (line = br.readLine()) != null; ) {
 			    	uris.add(line);
@@ -71,24 +71,25 @@ public class LearningAnalysisMain {
 			}
 			catch(Exception e) {
 				System.err.println("Error while reading URI file: " + e.getMessage());
+				return;
 			}
-			
+
 			/* Analyze all projects. */
 			for(String uri : uris) {
 
 				try {
 					gitProjectAnalysis = GitProjectAnalysis.fromURI(uri, LearningAnalysisMain.CHECKOUT_DIR, runner);
-				} 
-				catch(GitProjectAnalysisException e) {
-					LearningAnalysisMain.printUsage(e.getMessage(), parser);
-					return;
 				}
-				
+				catch(GitProjectAnalysisException e) {
+					e.printStackTrace(System.err);
+					continue;
+				}
+
 				/* Perform the analysis (this may take some time) */
 				gitProjectAnalysis.analyze();
-				
+
 			}
-			
+
 		}
 		else {
 			System.out.println("No repository given.");
@@ -111,7 +112,7 @@ public class LearningAnalysisMain {
         System.out.println("");
         return;
 	}
-	
+
 	/**
 	 * Prints the usage of main.
 	 * @param error The error message that triggered the usage message.
