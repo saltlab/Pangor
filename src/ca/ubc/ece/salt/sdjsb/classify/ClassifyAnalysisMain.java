362dec54cccc2e56a33f042d32462fe5a8c6bcf6
Fernando Brito <email@fernandobrito.com>
2015-08-21 14:13:45 -0700

    Make the number of threads an argument, and not a constant
    Easier to change the value when running on the cluster

--- src/ca/ubc/ece/salt/sdjsb/classify/ClassifyAnalysisMain.java
+++ src/ca/ubc/ece/salt/sdjsb/classify/ClassifyAnalysisMain.java
@@ -24,9 +24,6 @@ public class ClassifyAnalysisMain {
 
 	protected static final Logger logger = LogManager.getLogger(LearningAnalysisMain.class);
 
-	/** The size of the thread pool */
-	private static final int THREAD_POOL_SIZE = 7;
-
 	/** The directory where repositories are checked out. **/
 	public static final String CHECKOUT_DIR =  new String("repositories");
 
@@ -99,7 +96,7 @@ public class ClassifyAnalysisMain {
 			 * would mean all git project initializations would have to happen
 			 * before starting the analysis.
 			 */
-			ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+			ExecutorService executor = Executors.newFixedThreadPool(options.getNThreads());
 			CountDownLatch latch = new CountDownLatch(uris.size());
 
 			/* Analyze all projects. */
--- src/ca/ubc/ece/salt/sdjsb/learning/LearningAnalysisOptions.java
+++ src/ca/ubc/ece/salt/sdjsb/learning/LearningAnalysisOptions.java
@@ -6,22 +6,29 @@ public class LearningAnalysisOptions {
 
 	@Option(name="-u", aliases={"--uri"}, usage="The uri of the public repository (e.g., https://github.com/qhanam/JSRepairClass.git).")
 	private String host = null;
-	
+
 	@Option(name="-d", aliases={"--directory"}, usage="The git directory (e.g., /path/to/project/.git/).")
 	private String directory = null;
-	
+
 	@Option(name="-h", aliases={"--help"}, usage="Display the help file.")
 	private boolean help = false;
-	
+
 	@Option(name="-r", aliases={"--repositories"}, usage="The path to the file specifying the repositories to analyze.")
 	private String repoFile = null;
-	
+
 	@Option(name="-ds", aliases={"--dataset"}, usage="The data set file to read.")
 	private String dataSetPath = null;
-	
+
 	@Option(name="-s", aliases={"--supplement"}, usage="The folder path to place any supplementary files.")
 	private String supplement = null;
-	
+
+	@Option(name = "-tr", aliases = { "--threads" }, usage = "The number of threads to be used.")
+	private Integer nThreads = 6;
+
+	public Integer getNThreads() {
+		return this.nThreads;
+	}
+
 	public String getURI() {
 		return this.host;
 	}
@@ -29,19 +36,19 @@ public class LearningAnalysisOptions {
 	public String getGitDirectory() {
 		return this.directory;
 	}
-	
+
 	public boolean getHelp() {
 		return this.help;
 	}
-	
+
 	public String getRepoFile() {
 		return this.repoFile;
 	}
-	
+
 	public String getDataSetPath() {
 		return this.dataSetPath;
 	}
-	
+
 	public String getSupplementaryFolder() {
 		return this.supplement;
 	}
